49591bdd49c98fddeeffe920fe61f7081ca664c8
SBritmanP <79532500+SBritmanP@users.noreply.github.com>
2021-02-23 21:25:35 -0500

    Add files via upload

--- database/tablas.sql
+++ database/tablas.sql
@@ -1,22 +1,72 @@
--- primera tabla
-USE finesi_virtual;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+-- Britman Salcedo 
+-- Servidor: 127.0.0.1
+-- Versión del servidor: 10.3.15-MariaDB
+-- Versión de PHP: 7.1.30
 
-CREATE TABLE docente(
-  dni int PRIMARY KEY NOT NULL,
-  codigoD varchar(10),
-  nombred varchar(40),
-  direciond text,
-  cursos int
-)
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
--- segunda tabla tabla
-USE finesi_virtual;
-CREATE TABLE estudiante(
-  dnie int PRIMARY KEY NOT NULL,
-  codigoe varchar(10),
-  nombree varchar(40),
-  direcione text,
-  notas int,
-  semestre int
-)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `finesi_virtual`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `docente`
+--
+
+CREATE TABLE `docente` (
+  `dni` int(11) NOT NULL,
+  `codigoD` varchar(10) DEFAULT NULL,
+  `nombred` varchar(40) DEFAULT NULL,
+  `direciond` text DEFAULT NULL,
+  `cursos` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `dnie` int(11) NOT NULL,
+  `codigoe` varchar(10) DEFAULT NULL,
+  `nombree` varchar(40) DEFAULT NULL,
+  `direcione` text DEFAULT NULL,
+  `notas` int(11) DEFAULT NULL,
+  `semestre` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`dni`);
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`dnie`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
